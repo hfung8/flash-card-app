90b49387f01dd815f5777b924e3faa9ef229a85b
Harrison Fung <harrison.hok.fung@gmail.com>
2020-08-16 13:46:19 -0400

    added flash conatiener css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Flashcardlist from './Flashcardlist.js';
 import './App.css';
 
 function App() {
   const [flashcards, setFlashCards] = useState(SAMPLE_FLASHCARDS)
+  
+  useEffect(()=> {
+    axios.get('https://opentdb.com/api.php?amount=10&category=18&difficulty=easy')
+    .then(res => {
+      setFlashCards(res.data.results.map((questionItem, index) => { 
+        const answer = decodeString(questionItem.correct_answer);
+        const options = [
+          ...questionItem.incorrect_answers.map(a => decodeString(a)),
+           answer];
+        return {
+          id: `($index)-${Date.now()}`,
+          question: decodeString(questionItem.question),
+          answer: answer,
+          options: options.sort(() => Math.random() * .5),
+        }
+      })
+    )})
+  }, []);
+
+  function decodeString(str){
+    const textArea = document.createElement('textarea');
+    textArea.innerHTML = str;
+    return textArea.value;
+  }
+
   return (
+    <div className="container">
     <Flashcardlist flashcards={flashcards} />
+    </div>
   );
 }
 
